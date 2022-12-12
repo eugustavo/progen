1ff11cc0bf1e2438d769eac7fed0292343a4809e
eugustavo <gustavo.ifto@gmail.com>
2022-12-12 01:56:13 -0300

    chore: Include vite template

---
+++ templates/react/vite/src/App.tsx
@@ -0,0 +1,31 @@
+function App() {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        height: '100vh',
+
+        alignItems: 'center',
+        justifyContent: 'center',
+
+        flexDirection: 'column',
+      }}
+    >
+      <h3>Project created from the CLI</h3>
+      <div
+        style={{
+          padding: 10,
+          fontStyle: 'italic',
+          backgroundColor: '#f5e5b9',
+          borderRadius: 8,
+        }}
+      >
+        <span>
+          npx @eugustavo/progen
+        </span>
+      </div>
+    </div>
+  )
+}
+
+export default App
